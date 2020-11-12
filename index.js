ac4f93e9fa1fab39f8b212feb6c87cf027010d54
Shane Burns <mcshaneburns@gmail.com>
2020-11-12 16:16:46 -0500

    create package.json for future npm management - create index.js with basic premise and purpose in comments

---
+++ index.js
@@ -0,0 +1,6 @@
+// Import all classes, assets, dependencies, etc. that are required for the game to run
+
+// Create an init function to return the game wrapper for use in projects
+exports.init = function(){
+    //return ORO.init();
+}
