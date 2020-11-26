570f84332921b5403e11bb770e7ac8b58b7fbb05
Shane Burns <mcshaneburns@gmail.com>
2020-11-26 00:41:35 -0500

    interpolate scene position offset for smoother, roomier feel

--- build/OURO.module.js
+++ build/OURO.module.js
@@ -328,9 +328,10 @@ class PlayableScene extends SceneSkeleton{
         this.player = null;
     }
     update(){
+        let t = 1.0 - Math.pow(0.001, this.creation.tickDelta);
         let inversePos = this.player.body.position.clone();
         inversePos = inversePos.scale(-1);
-        this.scene.position.set(inversePos.x, inversePos.y, inversePos.z);
+        this.scene.position.lerp(new THREE.Vector3(inversePos.x, inversePos.y, inversePos.z), t);
     }
 }
 
